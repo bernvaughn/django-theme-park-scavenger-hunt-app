d33a5707514122e4da5d773d3a03314678be3314
Vaughn B <vaughnjbernard@gmail.com>
2024-01-17 18:22:36 -0500

    registered models

--- parkchallenge/parkchallenge/apps/scavengerhunt/admin.py
+++ parkchallenge/parkchallenge/apps/scavengerhunt/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from parkchallenge.apps.scavengerhunt import models
 
-# Register your models here.
+# TODO: create inlines and stuff
+
+admin.site.register(models.Board)
+admin.site.register(models.Task)
+admin.site.register(models.TaskToBoard)
